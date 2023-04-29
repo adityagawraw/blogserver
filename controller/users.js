eeff0d23b8bbe0c135dbb9d5dd4156815dc8001d
adityagawraw <adityagawraw006@gmail.com>
2023-04-30 02:37:16 +0530

    user auth

--- controller/users.js
+++ controller/users.js
@@ -1,28 +1,43 @@
-const User = require("../models/user")
+const User = require("../models/user");
 
-const createUser = async(req, res)=>{
-    try {
-        const user = await User.create(req.body);
-        res.json(user);
-    } catch (error) {
-        console.log(error)
+const createUser = async (req, res) => {
+  try {
+    const user = await User.create(req.body);
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+//get ogin email and password
+const getOneUser = async (req, res) => {
+  try {
+    const user = await User.findOne(req.body);
+    if (!user) {
+      res.json({ message: "no match" });
+    } else {
+      res.json({ message: "match", user: user });
     }
-}
-const getAllUsers = async(req, res)=>{
-    try {
-        const user = await User.find();
-        res.json(user);
-    } catch (error) {
-        console.log(error)
-    }
-}
-const deleteUserByID= async(req, res)=>{
-    try {
-        const user = await User.deleteOne({_id:req.params.id});
-        res.json(user);
-    } catch (error) {
-        console.log(error)
-    }
-}
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-module.exports = {createUser, getAllUsers, deleteUserByID}
+const getAllUsers = async (req, res) => {
+  try {
+    const user = await User.find();
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+const deleteUserByID = async (req, res) => {
+  try {
+    const user = await User.deleteOne({ _id: req.params.id });
+    res.json(user);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { createUser, getAllUsers, deleteUserByID, getOneUser };
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -1,8 +1,9 @@
 const  express = require("express");
 const router = express.Router();
-const  {createUser, getAllUsers, deleteUserByID} = require("../controller/users");
+const  {createUser, getAllUsers, deleteUserByID, getOneUser} = require("../controller/users");
 
-router.route('/').get(getAllUsers).post(createUser);
+router.route('/').get(getOneUser).post(createUser);
+
 router.route('/:id').delete(deleteUserByID);
 
 module.exports = router;
