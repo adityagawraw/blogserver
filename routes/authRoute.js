724baec8cc001dbb07d2dd4c5cc3de396ad2c9b6
adityagawraw <adityagawraw006@gmail.com>
2023-04-30 14:24:53 +0530

    post in login

--- routes/authRoute.js
+++ routes/authRoute.js
@@ -2,8 +2,8 @@ const  express = require("express");
 const router = express.Router();
 const  {createUser, getAllUsers, deleteUserByID, getOneUser} = require("../controller/users");
 
-router.route('/').get(getOneUser).post(createUser);
-
+router.route('/').post(createUser);
+router.route('/login').post(getOneUser)
 router.route('/:id').delete(deleteUserByID);
 
 module.exports = router;
